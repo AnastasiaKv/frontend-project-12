dce3d5555780efb02abbcdf2957febb0e6069561
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-09-13 18:53:31 +0300

    add locales

--- frontend/src/locales/en.js
+++ frontend/src/locales/en.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  translation: {
+    logout: 'Log Out',
+    loginPage: {
+      login: 'Log In',
+      password: 'Password',
+      username: 'Username',
+      required: 'Required field',
+      authFaild: 'Invalid username or password',
+      noAccaount: 'Don\'t have an account? ',
+      registration: 'Registration',
+    },
+  },
+};
--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  translation: {
+    logout: 'Выйти',
+    loginPage: {
+      login: 'Войти',
+      password: 'Пароль',
+      username: 'Ваш ник',
+      required: 'Поле обязательно для заполнения',
+      authFaild: 'Неверные имя пользователя или пароль',
+      noAccaount: 'Нет аккаунта? ',
+      registration: 'Регистрация',
+    },
+  },
+};
