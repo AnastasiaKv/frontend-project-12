e784a0bb206c6d56e0a588d74f0602a5a6f9763b
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-11-25 17:51:16 +0300

    refactor index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import usePromise from 'react-promise';
-import { Provider } from 'react-redux';
-import './assets/index.scss';
 import init from './init';
-import store from './slices/index';
 
 const InitApp = () => {
   const { value, loading } = usePromise(init);
@@ -12,8 +9,4 @@ const InitApp = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(
-  <Provider store={store}>
-    <InitApp />
-  </Provider>,
-);
+root.render(<InitApp />);
