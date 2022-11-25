7e180e251c1ed522742fcd5bf40e34fe364f8462
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-11-25 17:56:10 +0300

    add notfound page

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ReactComponent as Avatar } from '../assets/avatar_notfound.svg';
+import routes from '../routes';
 
-const NotFoundPage = () => <div>Page Not Found</div>;
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center">
+      <Avatar className="img-fluid h-25" />
+      <h1 className="h4 text-muted">
+        {t('notFoundPage.title')}
+      </h1>
+      <p className="text-muted">
+        {t('notFoundPage.message')}
+        <a href={routes.chatPage()}>{t('notFoundPage.homeLink')}</a>
+      </p>
+    </div>
+  );
+};
 
 export default NotFoundPage;
