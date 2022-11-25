d8442e3b710c4c9ec80fa1ccaf47f0d98136cf57
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-11-25 17:52:46 +0300

    refactor app component

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -11,25 +11,41 @@ import {
   Button, Navbar, Container,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { ToastContainer } from 'react-toastify';
 import RegistrationPage from './RegistrationPage';
 import LoginPage from './LoginPage';
 import ChatPage from './ChatPage';
 import NotFoundPage from './NotFoundPage';
 import AuthContext from '../contexts';
 import useAuth from '../hooks';
+import routes from '../routes';
 
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const logIn = () => setLoggedIn(true);
+  const logIn = (userData) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    setLoggedIn(true);
+  };
+
   const logOut = () => {
-    localStorage.removeItem('userId');
+    localStorage.removeItem('user');
     setLoggedIn(false);
   };
 
+  const getAuthHeader = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.token) {
+      return { Authorization: `Bearer ${user.token}` };
+    }
+    return {};
+  };
+
   const authContextProviderValue = useMemo(() => (
-    { loggedIn, logIn, logOut }
-  ), [loggedIn, logIn, logOut]);
+    {
+      loggedIn, logIn, logOut, getAuthHeader,
+    }
+  ), [loggedIn]);
 
   return (
     <AuthContext.Provider value={authContextProviderValue}>
@@ -38,21 +54,26 @@ const AuthProvider = ({ children }) => {
   );
 };
 
-const AuthButton = () => {
-  const auth = useAuth();
-  const { t } = useTranslation();
-
-  return (
-    auth.loggedIn && <Button onClick={auth.logOut}>{t('logout')}</Button>
-  );
-};
-
 const ChatRoute = ({ children }) => {
   const auth = useAuth();
   const location = useLocation();
 
   return (
-    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+    auth.loggedIn ? children : <Navigate to={routes.loginPage()} state={{ from: location }} />
+  );
+};
+
+const ChatNavbar = () => {
+  const auth = useAuth();
+  const { t } = useTranslation();
+
+  return (
+    <Navbar className="shadow-sm" bg="white" variant="ligth" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to={routes.chatPage()}>{t('brand')}</Navbar.Brand>
+        {auth.loggedIn && <Button onClick={auth.logOut}>{t('logout')}</Button>}
+      </Container>
+    </Navbar>
   );
 };
 
@@ -60,26 +81,21 @@ const App = () => (
   <AuthProvider>
     <div className="d-flex flex-column h-100">
       <Router>
-        <Navbar className="shadow-sm" bg="white" variant="ligth" expand="lg">
-          <Container>
-            <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-            <AuthButton />
-          </Container>
-        </Navbar>
-
+        <ChatNavbar />
         <Routes>
           <Route
-            path="/"
+            path={routes.chatPage()}
             element={(
               <ChatRoute>
                 <ChatPage />
               </ChatRoute>
             )}
           />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<RegistrationPage />} />
+          <Route path={routes.loginPage()} element={<LoginPage />} />
+          <Route path={routes.signupPage()} element={<RegistrationPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
+        <ToastContainer />
       </Router>
     </div>
   </AuthProvider>
