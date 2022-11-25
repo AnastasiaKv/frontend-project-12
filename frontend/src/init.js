098b348a24a6a256bc70e4a1e210a7b840f5a503
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-11-25 17:50:44 +0300

    move store provider to init, add rollbar and leoprofanity

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import App from './components/App';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import * as leoProfanity from 'leo-profanity';
 import resources from './locales/index.js';
+import App from './components/App';
+import store from './slices/index';
+import setupRollbar from './rollbar';
+import './assets/index.scss';
 
 const init = async () => {
+  const ruDictionary = leoProfanity.getDictionary('ru');
+  leoProfanity.add(ruDictionary);
+  const rollbarConfig = setupRollbar();
   const i18n = i18next.createInstance();
 
   await i18n
@@ -14,9 +24,18 @@ const init = async () => {
     });
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <Provider store={store}>
+          <RollbarProvider config={rollbarConfig}>
+            <ErrorBoundary>
+              <App />
+            </ErrorBoundary>
+          </RollbarProvider>
+        </Provider>
+      </I18nextProvider>
+    </React.StrictMode>
+
   );
 };
 
