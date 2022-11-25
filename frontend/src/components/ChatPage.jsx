282a4bd66ea6092c9cc9bd560c12cf6b15981bea
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-11-25 17:54:45 +0300

    refactor chat page

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,37 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import axios from 'axios';
 import routes from '../routes';
 import Channels from './Channels';
-
-const getAuthHeader = () => {
-  const userId = JSON.parse(localStorage.getItem('userId'));
-  if (userId && userId.token) {
-    return { Authorization: `Bearer ${userId.token}` };
-  }
-  return {};
-};
+import ChatRoom from './ChatRoom';
+import useAuth from '../hooks';
+import getModal from './modals/index.js';
+import { actions as channelsActions } from '../slices/channelsSlice';
 
 const ChatPage = () => {
-  const [content, setContent] = useState({});
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const auth = useAuth();
+  const { isOpened, type } = useSelector((state) => state.modal);
+  const Modal = getModal(type);
 
   useEffect(() => {
-    axios.get(routes.getDataPath(), { headers: getAuthHeader() })
-      .then((response) => {
-        console.log(response.data);
-        setContent(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log(content);
-  }, []);
+    const notify = (errorType) => toast.error(t(`errors.${errorType}`));
+
+    axios.get(routes.getDataPath(), { headers: auth.getAuthHeader() })
+      .then((response) => dispatch(channelsActions.setChannels(response.data)))
+      .catch((error) => notify(error.isAxiosError ? 'network' : 'unknown'));
+  }, [dispatch, auth, t]);
 
   return (
-    <div className="container h-100 my-4 overflow-hidden rounded shadow">
-      <div className="row h-100 bg-white flex-md-row">
-        <Channels />
+    <>
+      <div className="container h-100 my-4 overflow-hidden rounded shadow">
+        <div className="row h-100 bg-white flex-md-row">
+          <Channels />
+          <ChatRoom />
+        </div>
       </div>
-    </div>
+      { isOpened && <Modal />}
+    </>
   );
 };
 
