1db3a9ff3981d1a0e8422d03741e44a020d0f396
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-01 21:56:10 +0300

    refactor index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import usePromise from 'react-promise';
+import { io } from 'socket.io-client';
 import init from './init';
 
-const InitApp = () => {
-  const { value, loading } = usePromise(init);
-  return loading ? null : value;
+const runApp = () => {
+  const URL = 'http://localhost:5001';
+  const socket = io(URL);
+  const root = ReactDOM.createRoot(document.getElementById('chat'));
+
+  init(socket).then((app) => root.render(app));
 };
 
-const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(<InitApp />);
+runApp();
