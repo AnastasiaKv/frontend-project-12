f6c10038e5faa793636bcfe26f77afbb2d7d3ebd
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-01 21:55:14 +0300

    move providers to init component

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -4,17 +4,20 @@ import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import * as leoProfanity from 'leo-profanity';
+import { ToastContainer } from 'react-toastify';
 import resources from './locales/index.js';
+import { AuthProvider } from './contexts/AuthContext';
+import { SocketProvider } from './contexts/SocketContext';
 import App from './components/App';
 import store from './slices/index';
 import setupRollbar from './rollbar';
 import './assets/index.scss';
 
-const init = async () => {
-  const ruDictionary = leoProfanity.getDictionary('ru');
-  leoProfanity.add(ruDictionary);
+const init = async (socket) => {
   const rollbarConfig = setupRollbar();
   const i18n = i18next.createInstance();
+  const ruDictionary = leoProfanity.getDictionary('ru');
+  leoProfanity.add(ruDictionary);
 
   await i18n
     .use(initReactI18next)
@@ -29,13 +32,17 @@ const init = async () => {
         <Provider store={store}>
           <RollbarProvider config={rollbarConfig}>
             <ErrorBoundary>
-              <App />
+              <SocketProvider socket={socket}>
+                <AuthProvider>
+                  <App />
+                  <ToastContainer />
+                </AuthProvider>
+              </SocketProvider>
             </ErrorBoundary>
           </RollbarProvider>
         </Provider>
       </I18nextProvider>
     </React.StrictMode>
-
   );
 };
 
