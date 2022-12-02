36eff03471d230e0e442daa07e81a67e99bba1cc
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-03 01:21:18 +0300

    fix port

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import { io } from 'socket.io-client';
 import init from './init';
 
 const runApp = () => {
-  console.log(process.env.PORT);
   const URL = `http://localhost:${process.env.PORT || '5001'}`;
   const socket = io(URL);
   const root = ReactDOM.createRoot(document.getElementById('chat'));
