ebdd94ec5c7b4d6220f2df9172c88e3d1369cd15
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-03 02:12:22 +0300

    fix port

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,6 +3,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use(
     '/api',
-    createProxyMiddleware({ target: 'http://0.0.0.0:5001', changeOrigin: true }),
+    createProxyMiddleware({ target: `http://0.0.0.0:${process.env.PORT || '5001'}`, changeOrigin: true }),
   );
 };
