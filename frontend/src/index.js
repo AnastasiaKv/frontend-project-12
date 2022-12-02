b59353e3d682bcf32705e7362732e68e9776a98f
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-02 23:52:52 +0300

    try fix port

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import { io } from 'socket.io-client';
 import init from './init';
 
 const runApp = () => {
-  const URL = 'http://localhost:5001';
+  console.log(process.env.PORT);
+  const URL = `http://localhost:${process.env.PORT || '5001'}`;
   const socket = io(URL);
   const root = ReactDOM.createRoot(document.getElementById('chat'));
 
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const port = process.env.PORT || '5001';
-
 module.exports = (app) => {
   app.use(
     '/api',
-    createProxyMiddleware({ target: `http://0.0.0.0:${port}`, changeOrigin: true }),
+    createProxyMiddleware({ target: `http://0.0.0.0:${process.env.PORT || '5001'}`, changeOrigin: true }),
   );
 };
