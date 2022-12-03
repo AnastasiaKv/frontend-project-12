7988524680f37875db4500f2a1276d00ebfe574e
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-03 11:28:37 +0300

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import { io } from 'socket.io-client';
 import init from './init';
 
 const runApp = () => {
-  const URL = `http://localhost:${process.env.PORT || '5001'}`;
-  const socket = io(URL);
+  // const URL = `http://localhost:${process.env.PORT || '5001'}`;
+  const socket = io();
   const root = ReactDOM.createRoot(document.getElementById('chat'));
 
   init(socket).then((app) => root.render(app));
