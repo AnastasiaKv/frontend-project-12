98bd062438ddc906007ccec4009d2079274c8944
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-12-09 14:14:11 +0300

    refactor proxy setup

--- frontend/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = (app) => {
-  app.use(
-    '/api',
-    createProxyMiddleware({ target: `http://0.0.0.0:${process.env.PORT || '5001'}`, changeOrigin: true }),
-  );
-};
